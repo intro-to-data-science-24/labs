a8c2323907b6151dec21fc2e96c725ff01146ab9
Tom Arend <tom_arend@hotmail.de>
2022-11-01 09:22:38 +0100

    Fixed typos

--- session-7-web-scraping/7-web-scraping.R
+++ session-7-web-scraping/7-web-scraping.R
@@ -1,4 +1,4 @@
-## ----setup, include=FALSE--------------
+## ----setup, include=FALSE----------
 knitr::opts_chunk$set(echo = TRUE, message = FALSE, warning = FALSE)
 
 
@@ -6,20 +6,20 @@ knitr::opts_chunk$set(echo = TRUE, message = FALSE, warning = FALSE)
 knitr::include_graphics("pics/workflow.png")
 
 
-## ---- message=F------------------------
+## ---- message=F--------------------
 library(rvest)
 library(stringr)
+library(tidyverse)
 
 
-## --------------------------------------
+## ----------------------------------
 parsed_url <- read_html("https://en.wikipedia.org/wiki/Cologne")
 
 
-## --------------------------------------
-parsed_nodes <- html_nodes(parsed_url, 
-                           xpath = '//*[@id="mw-content-text"]/div[1]/p[75]')
-carnival <- html_text(parsed_nodes)
-carnival
+## ----------------------------------
+parsed_url %>% 
+  html_element(xpath = '//p[(((count(preceding-sibling::*) + 1) = 170) and parent::*)]') %>% 
+  html_text()
 
 
 ## ---- fig.align='center', echo=F, out.width = "90%"----
@@ -36,117 +36,188 @@ knitr::include_graphics("pics/selector.png")
 knitr::include_graphics("pics/selector2.png")
 
 
-## --------------------------------------
-hot100page <- "https://www.billboard.com/charts/hot-100"
-hot100 <- read_html(hot100page)
+## ----------------------------------
+nypl_url <- "https://www.nypl.org/books-more/recommendations/best-books/adults?year=2021"
+nypl100 <- read_html(nypl_url)
 
 
-## --------------------------------------
-rank <- hot100 %>% 
-  rvest::html_nodes('body') %>% 
-  xml2::xml_find_all("//span[contains(@class, 'chart-element__rank__number')]") %>% 
-  rvest::html_text()
+## ----------------------------------
+title <- nypl100 %>% 
+  html_elements(xpath = '//ul/li/div/div/h4') %>% 
+  html_text2()
 
-artist <- hot100 %>% 
-  rvest::html_nodes('body') %>% 
-  xml2::xml_find_all("//span[contains(@class, 'chart-element__information__artist')]") %>% 
-  rvest::html_text()
+author <- nypl100 %>% 
+  html_elements(css = '.spbb-card__byline--grid') %>% 
+  html_text2()
 
-title <- hot100 %>% 
-  rvest::html_nodes('body') %>% 
-  xml2::xml_find_all("//span[contains(@class, 'chart-element__information__song')]") %>% 
-  rvest::html_text()
-
-
-## --------------------------------------
-chart_df <- data.frame(rank, artist, title)
-
-knitr::kable(chart_df  %>% head(10))
-
-
-## --------------------------------------
-url_p <- read_html("https://en.wikipedia.org/wiki/List_of_Nobel_laureates_in_Literature")
-tables <- html_nodes(url_p, xpath = '//*[@id="mw-content-text"]/div[1]/table[3]')
-html_table(tables)
+summary <- nypl100 %>% 
+  html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "spbb-card__description--grid", " " ))]') %>% 
+  html_text2()
 
 
 
-spaceflights <- as.data.frame(tables[[1]])
+## ----------------------------------
+knitr::kable(data.frame(title, author, summary) %>% head(3))
 
-knitr::kable(spaceflights[,1:7]  %>% head(10))
+
+## ----------------------------------
+url_p <- read_html('https://en.wikipedia.org/wiki/R_(programming_language)')
+r_table <- html_table(url_p, header = TRUE, fill = TRUE) %>% 
+  pluck(2)
+r_table %>% 
+  select(Description) %>% 
+  head(5)
 
 
-## ---- eval=F---------------------------
+## ---- eval=F-----------------------
 ## browseURL("http://www.jstatsoft.org/issue/archive")
 
 
-## --------------------------------------
+## ----------------------------------
 baseurl <- "http://www.jstatsoft.org/article/view/v"
-volurl <- paste0("0", seq(1, 99, 1))
-volurl[1:9] <- paste0("00", seq(1, 9, 1))
-brurl <- paste0("0", seq(1, 9, 1))
-urls_list <- paste0(baseurl, volurl)
-urls_list <- paste0(rep(urls_list, each = 9), "i", brurl)
-urls_list[1:5]
+volurl <- paste0("0", 1:99)
+volurl[1:9] <- paste0("00", 1:9)
+brurl <- paste0("0", 1:9)
+
+urls_list <- cross2(volurl, brurl) %>% map_chr(~paste0(baseurl, .[[1]], 'i', .[[2]]))
+names <- cross2(volurl, brurl) %>% map_chr(~paste0(.[[1]], '_', .[[2]], '.html'))
 
 
-## ---- eval=F---------------------------
+## ---- eval=F-----------------------
 ## tempwd <- ("data/jstatsoftStats")
 ## dir.create(tempwd)
 ## setwd(tempwd)
 
 
-## ---- eval=F---------------------------
-## folder <- "html_articles/"
+## ---- eval=F-----------------------
+## folder <- paste0(tempwd, "\\html_articles\\")
 ## dir.create(folder)
 ## 
-## for (i in 1:length(urls_list)) {
+## for (i in seq_along(urls_list)) {
 ##   # only update, don't replace
-##     if (!file.exists(paste0(folder, names[i]))) {
-##   # skip article when we run into an error
-##       tryCatch(
-##         download.file(urls_list[i], destfile = paste0(folder, names[i])),
-##         error = function(e) e
-##       )
-##   # don't kill their server --> be polite!
-##       Sys.sleep(runif(1, 0, 1))
-## 
-## } }
+##   if (!file.exists(paste0(folder, names[i]))) {
+##     # skip article when we run into an error
+##     tryCatch(
+##       download.file(urls_list[i], destfile = paste0(folder, names[i])),
+##       error = function(e)
+##         e
+##     )
+##     # don't kill their server --> be polite!
+##     Sys.sleep(runif(1, 0, 1))
+##   }
+## }
 
 
 ## ---- fig.align='center', echo=F, out.width = "70%"----
 knitr::include_graphics("pics/html_files.png")
 
 
-## ---- eval=F---------------------------
+## ---- eval=F-----------------------
 ## list_files <- list.files(folder, pattern = "0.*")
 ## list_files_path <- list.files(folder, pattern = "0.*", full.names = TRUE)
 ## length(list_files)
 
 
-## ---- eval=F---------------------------
+## ---- eval=F-----------------------
 ## # define output first
 ## authors <- character()
 ## title <- character()
 ## datePublish <- character()
 ## 
 ## # then run the loop
-## for (i in 1:length(list_files_path)) {
+## for (i in seq_along(list_files_path)) {
+## 
 ##   html_out <- read_html(list_files_path[i])
 ## 
-##   authors[i] <- html_text(html_nodes(html_out , xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "authors_long", " " ))]//strong'))
+##   authors[i] <- html_out %>%
+##     html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "authors_long", " " ))]//strong') %>%
+##     html_text2()
 ## 
-##   title[i] <- html_text(html_nodes(html_out , xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "page-header", " " ))]'))
+##   title[i] <- html_out %>%
+##     html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "page-header", " " ))]') %>%
+##     html_text2()
 ## 
-##   datePublish[i] <- html_text(html_nodes(html_out , xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "article-meta", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "row", " " )) and (((count(preceding-sibling::*) + 1) = 2) and parent::*)]//*[contains(concat( " ", @class, " " ), concat( " ", "col-sm-8", " " ))]'))
+##   datePublish[i] <- html_out %>%
+##     html_elements(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "article-meta", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "row", " " )) and (((count(preceding-sibling::*) + 1) = 2) and parent::*)]//*[contains(concat( " ", @class, " " ), concat( " ", "col-sm-8", " " ))]') %>%
+##     html_text2()
+## 
 ## }
 ## 
 ## # inspect data
 ## authors[1:3]
-## title[1:2]
+## title[1:3]
 ## datePublish[1:3]
 ## 
 ## # create a data frame
 ## dat <- data.frame(authors = authors, title = title, datePublish = datePublish)
 ## dim(dat)
+
+
+## ----------------------------------
+library(httr)
+library(jsonlite)
+library(xml2)
+
+
+## ----------------------------------
+baseurl <- "https://swapi.dev/api/"
+
+query <- 'films'
+
+GET(paste0(baseurl, query)) %>%
+  content(as = 'text') %>%
+  fromJSON() %>% 
+  pluck(4) %>% 
+  as.data.frame()
+  
+
+
+## ----------------------------------
+query <- 'people/?search=skywalker'
+
+GET(paste0(baseurl, query)) %>%
+  content(as = 'text') %>%
+  fromJSON() %>% 
+  pluck(4) %>% 
+  as.data.frame()
+
+
+## ----------------------------------
+query <- 'starships/?search=death/?format=wookiee'
+
+GET(paste0(baseurl, query)) %>%
+  http_type()
+
+
+## ---- eval=FALSE-------------------
+## baseurl <- 'https://api.congress.gov/v3/'
+## 
+## query <- 'nomination/115/2259/actions'
+## 
+## GET(paste0(baseurl, query),
+##     authenticate(user = Sys.getenv('US.GOV_API'), password = '')) %>%
+##   content(as = 'text') %>%
+##   fromJSON() %>%
+##   pluck(1) %>%
+##   as.data.frame()
+
+
+## ---- eval=FALSE-------------------
+## query <- paste0('summaries/117/hr?fromDateTime=2022-10-01T00:00:00Z&toDateTime=2022-11-01T00:00:00Z&sort=updateDate+desc')
+## 
+## GET(paste0(baseurl, query), query = list(api_key = Sys.getenv('US.GOV_API'))) %>%
+##   content(as = 'text') %>%
+##   fromJSON() %>%
+##   pluck(3) %>%
+##   as.data.frame()
+
+
+## ---- eval=FALSE-------------------
+## query <- 'member/O000167/sponsored-legislation.xml'
+## 
+## GET(paste0(baseurl, query),
+##     add_headers('X-Api-Key' = Sys.getenv('US.GOV_API'))) %>%
+##   content(as = 'text') %>%
+##   read_xml() %>%
+##   xml_find_all('//sponsoredLegislation//title') %>% xml_text()
 
